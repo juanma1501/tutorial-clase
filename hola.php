439b3c2b5472233fdf1014f85b76bbdd203b98e1
juanma1501 <juanmaporreroalmansa@gmail.com>
2024-10-28 12:30:49 +0100

    Programa interactivo

--- hola.php
+++ hola.php
@@ -1,4 +1,22 @@
 <?php
-// El nombre por defecto es mundo
-$nombre = isset($argv[1]) ? argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+
+print
+"Introduce tu nombre:"
+;
+$nombre
+=
+trim
+(
+fgets
+(
+STDIN
+));
+@
+print
+"Hola,
+{
+$nombre
+}
+\n
+"
+;
